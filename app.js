81f70da522e85555caee6569e20a83fbd5859f4c
sinkerdPhoutthavong <devn.info@gmail.com>
2019-04-01 01:09:43 +0700

    create del cookie

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 var express = require('express');
-var app = express();
 var body = require('body-parser');
+var cookie = require('cookie-parser');
+var app = express();
 app.use(body());
 app.set('view engine','ejs');
 
-app.get('/showForm',function(req,res){
-    res.sendFile(__dirname+"/"+"showForm.html");
+app.get('/createCookie',function(req,res){
+    res.cookie('myCookie','Sinkerd Devn');
+    res.end("Create Cookie");
+});
+app.get('/delCookie',function(req,res){
+    res.clearCookie('myCookie');
+    res.end('Delete Cookie Complete');
 });
 
-app.post('/showData',function(req,res){ // for post get  first install body-parser
-    data={
-        fname:req.body.fname,
-        lname:req.body.lname
-    }
-    console.log(data);
-    res.end("<h1>"+data.fname+"</h1>");
-});
+// app.get('/showForm',function(req,res){ // for route
+//     res.sendFile(__dirname+"/"+"showForm.html");
+// });
+
+// app.post('/showData',function(req,res){ // for post get  first install body-parser
+//     data={
+//         fname:req.body.fname,
+//         lname:req.body.lname
+//     }
+//     console.log(data);
+//     res.end("<h1>"+data.fname+"</h1>");
+// });
 
 // app.get('/showData',function(req,res){ // for method get
 //     data={
