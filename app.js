33214cdfe2a9451fd52c31f6c03a9ba9e59ea917
sinkerdPhoutthavong <devn.info@gmail.com>
2019-04-01 00:59:54 +0700

    get, post must to install body-parser to show data.... klnow route show page like page easy

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-var fs = require('fs');
-var Readme = fs.readFileSync('./code.txt','UTF-8');
-fs.mkdir('Project',function(){
-    fs.writeFileSync('./Project/Readme.txt',Readme);
-    var a = "Success";
-    console.log(a);
+var express = require('express');
+var app = express();
+var body = require('body-parser');
+app.use(body());
+app.set('view engine','ejs');
+
+app.get('/showForm',function(req,res){
+    res.sendFile(__dirname+"/"+"showForm.html");
 });
 
+app.post('/showData',function(req,res){ // for post get  first install body-parser
+    data={
+        fname:req.body.fname,
+        lname:req.body.lname
+    }
+    console.log(data);
+    res.end("<h1>"+data.fname+"</h1>");
+});
+
+// app.get('/showData',function(req,res){ // for method get
+//     data={
+//         fname:req.query.fname,
+//         lname:req.query.lname
+//     }
+//     console.log(data);
+//     res.end(JSON.stringify(data));
+// });
 
 
+
+// app.get('/',function(req,res){
+//     var data ={name:"Devn Infor ",age: 20,job: "Programer",}
+//     res.render('profile',{user:data});
+// });
+// app.get('/contact',function(req,res){
+//     res.render('contact');
+// });
+// app.get('/database',function(req,res){
+//     res.render('database');
+// });
+app.listen(8081);
 
 
